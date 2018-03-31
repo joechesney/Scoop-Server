b62bec5c9604f248298f30c98d3aae8072a3e397
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-31 16:52:21 -0500

    added files that i will need later on. set up comments describing what each file will do

---
+++ scoopers/getPrices.js
@@ -0,0 +1,2 @@
+// This file will mainly contain a function that grabs
+// the average prices from "https://reverb.com/price-guide/guide/8799-electro-harmonix-little-big-muff-pi"
---
+++ scoopers/getWatchList.js
@@ -0,0 +1,3 @@
+
+// This file will mainly contain a functino to grab the 
+// watched items ids and names from the url "https://reverb.com/my/feed/customize"
