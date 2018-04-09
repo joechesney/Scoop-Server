3f88336fb519b51b5302f8a18bb4779a642d28b8
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-08 21:25:32 -0500

    still figuring out file structure

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -3,6 +3,6 @@
 const { Router } = require('express');
 const router = Router();
 
-router.use(require('../Reverb-Scoopers/index.js'));
+router.use(require('./Reverb-Scoopers/index.js'));
 
 module.exports = router;
