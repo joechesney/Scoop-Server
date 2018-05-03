45c8ecf1a9f3b46dda3e65a0a99171951b617abb
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-03 15:54:07 -0500

    finished refactor with loginAuth and getAPIData functions, they are passing in parameters to the gs

--- Backend/helpers/_getAPIData.js
+++ Backend/helpers/_getAPIData.js
@@ -1,10 +1,11 @@
 
 const request = require('request');
 
-module.exports.getAPIData = (accesss_token, url) => {
+module.exports = (access_token, urlSuffix) => {
   return new Promise((resolve, reject) => {
+
     let options = {
-      url: 'https://reverb.com/api/my/feed',
+      url: `https://reverb.com/api${urlSuffix}`,
       headers: {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
         'Authorization': `Bearer ${access_token}`
@@ -13,9 +14,13 @@ module.exports.getAPIData = (accesss_token, url) => {
       "Accept": "application/hal+json",
       "Accept-Version": "3.0",
     }
-    request(options, (error, response2, html2) => {
-      response2 = JSON.parse(response2.body); //THIS WORKS!
-      return response2;
+    request(options, (error, response, html) => {
+      if (!error) {
+        response = JSON.parse(response.body); //THIS WORKS!
+        resolve(response);
+      } else {
+        reject(error)
+      }
     })
   })
 }
--- Backend/helpers/_loginAuth.js
+++ Backend/helpers/_loginAuth.js
@@ -6,9 +6,16 @@ const secrets = require('./secrets');
 // add auth token to the header options object
 // use auth otken for all subsequent requests
 
-module.exports.loginAuth = () =>{
+// If I allow for other users to register with the site,
+// there will need to be a GET function that grabs the users
+// info from the database and uses currentUser.clientId
+// and currentUser.clientSecret as the variables in the
+// function below, INSTEAD of requiring in the secrets.js
+// file that I am using here right now
+
+module.exports = () =>{
   return new Promise((resolve, reject)=>{
-    let b = request.jar();
+
     let dataString = `grant_type=client_credentials&client_id=${secrets.clientId}&client_secret=${secrets.clientSecret}&scope=public+read_listings+read_orders+read_lists+write_lists+read_profile+write_profile`;
 
     let options = {
@@ -18,6 +25,7 @@ module.exports.loginAuth = () =>{
     };
 
     request(options, (error, response, html)=>{
+      console.log('loginAuth error: ',error);
       if (!error && response.statusCode == 200) {
         let bodyObj = JSON.parse(response.body);
         resolve(bodyObj);
