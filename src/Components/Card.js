8847920fe0494b8433f2a03a4165d907c456be8a
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-08 15:00:19 -0500

    printing more product info to the card in the DOM

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Card, Icon, Image } from 'semantic-ui-react'
 
-const ProductCard = props => (
+const ProductCard = props => {
+  const dateOfPublication = new Date(props.card.created_at).toLocaleString();
+  return (
 <a href={props.card._links.web.href}>
   <Card>
     <Image src={props.card.photos[0]._links.small_crop.href} />
@@ -11,27 +13,25 @@ const ProductCard = props => (
       </Card.Header>
       <Card.Meta>
         <span className='date'>
-          {props.card.published_at.toLocaleString()}
+          {dateOfPublication}
         </span>
       </Card.Meta>
       <Card.Description>
-        Matthew is a musician living in Nashville.
+        {props.card.condition}
+        {props.card.location.display_location}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
-
-        <Icon name='user' />
-
+      <Icon name='dollar' />
         {
           (props.card.SCOOP)&&(props.card.SCOOP.percentOfMarketPrice < 100)
           ? `${props.card.SCOOP.percentBelowMarketPrice} % below market price`
           : `0% below market price`
-          }
-
-
+        }
     </Card.Content>
   </Card>
   </a>
 )
+}
 
 export default ProductCard;
