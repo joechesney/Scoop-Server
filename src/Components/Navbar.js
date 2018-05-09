9e971446903e4975577b96daa74bfbeded105e76
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-09 14:26:23 -0500

    added a simple navbar to navigate between different pages

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link, BrowserRouter as Browser, Route } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <h3>SCOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOp</h3>
+    <ul>
+      <li><Link to="/" >Home</Link></li>
+      <li><Link to="/myfeed" >My Feed</Link></li>
+      <li><Link to="/mywatchlist" >My Watchlist</Link></li>
+    </ul>
   )
 }
 
