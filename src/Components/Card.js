28c874cc0ee7aa6718115e3ac5ad05d417fab055
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-10 14:01:10 -0500

    cards are currently displaying properly with the Card Component being defined as a variable above render () and passing it in as a variable. still need to fix the NaN for lowest avialbe price

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,24 +3,80 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 const ProductCard = props => {
   const dateOfPublication = new Date(props.card.created_at).toLocaleString();
-  const prices = props.card.SCOOP.hasPriceGuide === true
+  const wholeCard = props.card.SCOOP.hasPriceGuide === true
     ? (
+
+      <Card>
+        <Image src={props.card.photos[0]._links.small_crop.href} />
+        <Card.Content>
+          <Card.Header>
+            {props.card.make} {props.card.model}
+          </Card.Header>
+          <Card.Meta>
+            <span className='date'>
+              {dateOfPublication}
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            {props.card.condition}
+            {props.card.location.display_location}
+          </Card.Description>
+        </Card.Content>
         <Card.Content extra>
           <div>
             This Price: ${props.card.price.amount} + {props.card.shipping.rates[0].rate.display} shipping
           </div>
           <div>Avg. Price: ${props.card.SCOOP.avgMarketPrice.toFixed(2)}</div>
         </Card.Content>
+        <Card.Content extra>
+          <Icon name='dollar' />
+            {
+              (props.card.SCOOP)&&(props.card.SCOOP.percentOfMarketPrice < 100)
+              ? <span className="scoopPrice">${props.card.SCOOP.percentBelowMarketPrice} % below market price</span>
+              : <span className="notScoopPrice">${ (props.card.SCOOP.percentOfMarketPrice - 100).toFixed(0)}% above market price</span>
+            }
+        </Card.Content>
+      </Card>
+
+
     )
     :
 
     (
-      <Card.Content extra>
-        <div>
-          This Price: ${props.card.price.amount} + {props.card.shipping.rates[0].rate.display} shipping
-        </div>
-        <div>Lowest Available Price: ${props.card.SCOOP.lowestAvailable}</div>
-      </Card.Content>
+
+        <Card>
+          <Image src={props.card.photos[0]._links.small_crop.href} />
+          <Card.Content>
+            <Card.Header>
+              {props.card.make} {props.card.model}
+            </Card.Header>
+            <Card.Meta>
+              <span className='date'>
+                {dateOfPublication}
+              </span>
+            </Card.Meta>
+            <Card.Description>
+              {props.card.condition}
+              {props.card.location.display_location}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div>
+              This Price: ${props.card.price.amount} + {props.card.shipping.rates[0].rate.display} shipping
+            </div>
+            <div>Lowest Available Price: ${props.card.SCOOP.lowestAvailable}</div>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name='dollar' />
+              {
+                (props.card.SCOOP)&&(props.card.SCOOP.percentOfMarketPrice < 100)
+                ? <span className="scoopPrice">This is the lowest price available</span>
+                : <span className="notScoopPrice">${ (props.card.SCOOP.percentOfMarketPrice - 100).toFixed(0)}% above lowest available price</span>
+              }
+          </Card.Content>
+        </Card>
+
+
     );
     // :
     // (
@@ -36,37 +92,10 @@ const ProductCard = props => {
 
 
   return (
-<a href={props.card._links.web.href}>
-  <Card>
-    <Image src={props.card.photos[0]._links.small_crop.href} />
-    <Card.Content>
-      <Card.Header>
-        {props.card.make} {props.card.model}
-      </Card.Header>
-      <Card.Meta>
-        <span className='date'>
-          {dateOfPublication}
-        </span>
-      </Card.Meta>
-      <Card.Description>
-        {props.card.condition}
-        {props.card.location.display_location}
-      </Card.Description>
-    </Card.Content>
-
-      {prices}
-
-    <Card.Content extra>
-      <Icon name='dollar' />
-        {
-          (props.card.SCOOP)&&(props.card.SCOOP.percentOfMarketPrice < 100)
-          ? <span className="scoopPrice">${props.card.SCOOP.percentBelowMarketPrice} % below market price</span>
-          : <span className="notScoopPrice">${props.card.SCOOP.percentOfMarketPrice - 100}% above market price</span>
-        }
-    </Card.Content>
-  </Card>
-  </a>
-)
+    <a href={props.card._links.web.href}>
+      {wholeCard}
+    </a>
+  )
 }
 
 export default ProductCard;
