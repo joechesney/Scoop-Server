afece8fa8fc076449b750a2f2936cfd462dd438c
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-11 13:56:13 -0500

    added more component files that are unfinished (unstarted, actually) and set up their routes i think

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 // import axios from 'axios';
 import Container from './Components/Container';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import List from './Components/List';
+import Product from './Components/Product';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 
@@ -19,8 +22,12 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
+          <Route exact path="/" render={()=><Home endpoint={"http://localhost:3333/scoop/home"}/>} />
           <Route path="/myfeed" render={()=><Container endpoint={"http://localhost:3333/scoop/myfeed"}/>} />
           <Route path="/mywatchlist" render={()=><Container endpoint={"http://localhost:3333/scoop/mywatchlist"}/>} />
+          <Route path="/reverblists" render={()=><List endpoint={"http://localhost:3333/scoop/reverblists"}/>} />
+          <Route path="/scooplists" render={()=><List endpoint={"http://localhost:3333/scoop/scooplists"}/>} />
+          <Route path="/product" render={()=><Product endpoint={"http://localhost:3333/scoop/product"}/>} />
         </div>
       </Router>
     );
---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = (props) => {
+
+  return (
+    <div></div>
+  )
+}
+
+export default Home;
