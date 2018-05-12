01fcf3e7ce3306f332927c99f062e4c91940a70d
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-12 17:00:11 -0500

    got the twilio text message thing to work. it was a typo the whole time

--- Backend/helpers/sendText.js
+++ Backend/helpers/sendText.js
@@ -1,11 +1,12 @@
-const secrets = require('./secrets');
-const SMSclient = new require('twilio')(secrets.twilioAccountSID, secrets.twilioAuthToken);
 
-module.exports = (user) => {
-  client.messages.create({
+module.exports = () => {
+  let secrets = require('./secrets');
+  let SMSclient = new require('twilio')(secrets.twilioAccountSID, secrets.twilioAuthToken);
+  console.log('HELLO???!!!');
+  SMSclient.messages.create({
     body: 'Hello from Scoop!',
-    to: user.phoneNumber,  // Text this number
+    to: "+16154389707",  // Text this number
     from: secrets.twilioPhoneNumber, // From a valid Twilio number
   })
-  .then((message) => console.log(message.sid));
+  .then((error, message) => console.log("sendText.js", error || message.sid));
 }
