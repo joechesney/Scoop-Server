6cfad1314c152426c9b88240e700d47c2fabf457
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-20 09:37:47 -0500

    added some desciption comments to the _priceGuideHelper file

--- Backend/helpers/_compShopHelper.js
+++ Backend/helpers/_compShopHelper.js
@@ -30,7 +30,7 @@ module.exports = (productObj) =>{
     productObj.SCOOP.priceToDisplay = +productObj.price.amount;
     return productObj;
   } else {
-    console.log('Product without either used low price or new low price',productObj.id);
+    console.log('Product without either used low price or new low price: ',productObj.id);
     return productObj;
   }
 }
