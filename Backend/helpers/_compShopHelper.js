4e0456dafdae0f978722471c19d3facf14fb9834
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-20 09:27:31 -0500

    added comments to help describe how determineSituationHelper works

--- Backend/helpers/_compShopHelper.js
+++ Backend/helpers/_compShopHelper.js
@@ -1,16 +1,17 @@
 // Only products that do not have price-guide data will be passed
 // into this function. It should calculate the price based on the
-// caomparison_shopping property of the object and send back
+// comparison_shopping property of the object and send back
 // the productObj with that determined value as the productObj.SCOOP.priceToDisplay
 
 module.exports = (productObj) =>{
 
   if(+productObj.compShopData.used_low_price.amount > 0){
+
     let percentAboveLowestAvailable;
+
     // if the products price is above the used_low_price then show
     // its price compared to that low
     if(+productObj.price.amount > +productObj.compShopData.used_low_price.amount){
-      // let percentOfMarketPrice = 100 * (+productPrice/avgMarketPrice).toFixed(2);
 
       percentAboveLowestAvailable = (100*(+productObj.price.amount/productObj.compShopData.used_low_price.amount).toFixed(2));
 
