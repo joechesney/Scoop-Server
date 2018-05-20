97c9cbd1aef2afb877c55f97854bb6e3fa488640
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-20 09:13:17 -0500

    deleted the neither file which was unused and fixed 2 small errors inthe Card render component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -24,7 +24,7 @@ const ProductCard = props => {
         </Card.Content>
         <Card.Content extra>
           <div>
-            This Price: ${props.card.price.amount} + {shipping}
+            This Price: ${props.card.price.amount} + {shipping} S&H
           </div>
           <div>Avg. Price: ${props.card.SCOOP.avgMarketPrice.toFixed(2)}</div>
         </Card.Content>
@@ -32,7 +32,7 @@ const ProductCard = props => {
           <Icon name='dollar' />
             {
               (props.card.SCOOP)&&(props.card.SCOOP.percentOfMarketPrice < 100)
-              ? <span className="scoopPrice">${props.card.SCOOP.percentBelowMarketPrice} % below market price</span>
+              ? <span className="scoopPrice">{props.card.SCOOP.percentBelowMarketPrice} % below market price</span>
               : <span className="notScoopPrice">{ (props.card.SCOOP.percentOfMarketPrice - 100).toFixed(0)}% above market price</span>
             }
         </Card.Content>
@@ -58,7 +58,7 @@ const ProductCard = props => {
           </Card.Content>
           <Card.Content extra>
             <div>
-              This Price: ${props.card.price.amount} + {props.card.shipping.rates[0].rate.display} shipping
+              This Price: ${props.card.price.amount} + {shipping} S&H
             </div>
             <div>Lowest Available Price: ${props.card.SCOOP.lowestAvailable}</div>
           </Card.Content>
