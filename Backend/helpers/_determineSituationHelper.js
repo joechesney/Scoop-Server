3ab439e1821d2fd7be27d9e666067c9557f2feda
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-20 09:43:38 -0500

    cleaned up a few files

--- Backend/helpers/_determineSituationHelper.js
+++ Backend/helpers/_determineSituationHelper.js
@@ -4,12 +4,12 @@
 // determine what to do with this product
 // Every product will be run through this function first
 
-module.exports = (productObj) =>{
+module.exports = (productObj) => {
 
   // The first case is the ideal case
   // This will be true if the product itself has comparison
   // price data directly attached to it
-  if(productObj._embedded !== undefined){
+  if (productObj._embedded !== undefined) {
     productObj.SCOOP = {
       hasPriceGuide: true,
       hasCompShop: false,
@@ -21,7 +21,7 @@ module.exports = (productObj) =>{
     // These products do not have market price data attached to them,
     // but they do have a url attached that can be used to make another
     // ajax call where i can GET some market price data
-  } else if((productObj._embedded == undefined) && (productObj._links.comparison_shopping)){
+  } else if ((productObj._embedded == undefined) && (productObj._links.comparison_shopping)) {
     productObj.SCOOP = {
       hasPriceGuide: false,
       hasCompShop: true,
@@ -37,7 +37,7 @@ module.exports = (productObj) =>{
     // they are never rendered to the site. This was an executive decision
     // made to not mislead users who might see the "This is the lowest price available"
     // banner and think that the product is a good deal, when it is not
-  } else if((!productObj._embedded) && (!productObj._links.comparison_shopping)){
+  } else if ((!productObj._embedded) && (!productObj._links.comparison_shopping)) {
     productObj.SCOOP = {
       hasPriceGuide: false,
       hasCompShop: false,
@@ -49,6 +49,6 @@ module.exports = (productObj) =>{
     // If it's not returned by now, then this is an extremely rare
     // case, and this console log will let developers know about
     // this outlier product
-    console.log('This Product is an outlier and has not been returned by determineSituationHelper: ',productObj);
+    console.log('This Product is an outlier and has not been returned by determineSituationHelper: ', productObj);
   }
 }
