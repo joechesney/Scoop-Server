dd313763d0cba97f5d0d80125519d48fb81b66f6
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-28 10:05:46 -0500

    fixed strange error where react couldnt find my ShowMore Component even though it was right where I left it. I changed the prop name and it worked lol. Also pagination works on the reverbDeals page

--- Backend/controllers/reverbDealsCtrl.js
+++ Backend/controllers/reverbDealsCtrl.js
@@ -36,3 +36,41 @@ module.exports.assessReverbDeals = (req, res, next) => {
     })
   })
 }
+
+
+module.exports.showMoreProducts = (req, res, next) => {
+  console.log('req.body',req.body);
+
+  loginAuth().then(token => {
+    getProductsList(token.access_token, req.body.nextPage)
+    .then(dataFromAPI=>{
+      let promiseArray1 = [];
+      productsArray = [];
+      for(let i = 0; i < dataFromAPI.body.listings.length; i++){
+        promiseArray1.push(getSingleProduct(token.access_token, dataFromAPI.body.listings[i]._links.self.href));
+      }
+      Promise.all(promiseArray1).then(listings=>{
+        let promiseArray2 = [];
+        let listingsWithPriceGuideData = listings.filter((listing)=>listing.SCOOP.hasPriceGuide).map(listing=>priceGuideHelper(listing));
+
+        // i know this isnt being assigned to anything.
+        // im just doing it to create promiseArray2
+        listings
+        .filter((listing)=>listing.SCOOP.hasCompShop)
+        .map(listing=>{
+          promiseArray2.push(getCompShopData(token.access_token, listing));
+        });
+        Promise.all(promiseArray2)
+        .then(listingsWithCompShopData=>{
+          // sendText();
+          let allListings = listingsWithPriceGuideData.concat(listingsWithCompShopData);
+          res.send({
+            products: allListings,
+            nextPage: dataFromAPI.body._links.next.href,
+          });
+        })
+      })
+    })
+  })
+
+}
