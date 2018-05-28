ed936601fd0f95ed34cb8b713fc0c97559c22197
joechesney <33613119+joechesney@users.noreply.github.com>
2018-05-28 10:32:00 -0500

    added a controller JUST for pagination. the 3 main pages use it for paginaiton. scoopDeals does not need pagination because it searches for ALL results on pageload so it cannot add more products ever. need to remove the showMore button from that page

--- Backend/routes/Reverb-Scoopers/myFeedRoute.js
+++ Backend/routes/Reverb-Scoopers/myFeedRoute.js
@@ -1,12 +1,11 @@
+
 const feedRouter = require('express').Router();
 const { assessMyFeedPrices } = require('../../controllers/myFeedCtrl');
+// Note that the pagination controller is shared on 3 views
+const { showMoreProducts } = require('../../controllers/paginationCtrl');
 
 // "/myFeed"
 feedRouter.get('/', assessMyFeedPrices);
-
-feedRouter.post('/', (req, res, next) => {
-  console.log('post req',req.body);
-
-})
+feedRouter.post('/', showMoreProducts);
 
 exports = module.exports = feedRouter;
--- Backend/routes/Reverb-Scoopers/myWatchlistRoute.js
+++ Backend/routes/Reverb-Scoopers/myWatchlistRoute.js
@@ -1,7 +1,11 @@
+
 const watchlistRouter = require('express').Router();
 const { assessMyWatchlistPrices } = require('../../controllers/myWatchlistCtrl');
+// Note that the pagination controller is shared on 3 views
+const { showMoreProducts } = require('../../controllers/paginationCtrl.js');
 
 // "/mywatchlist"
 watchlistRouter.get('/', assessMyWatchlistPrices);
+watchlistRouter.post('/', showMoreProducts);
 
 exports = module.exports = watchlistRouter;
