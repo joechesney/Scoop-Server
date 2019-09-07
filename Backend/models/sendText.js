1b0a401c4116bceca59f94a39f2ff7c76f25e4b4
Joe Chesney <joedihchesney@gmail.com>
2019-09-07 11:30:42 -0500

    deleted all the secrets bc i dont remember why i hardcoded them in the first place

--- Backend/models/sendText.js
+++ Backend/models/sendText.js
@@ -1,13 +1,4 @@
 
-// let secrets = require('./secrets');
-let secrets = {
-  reverbClientId: "cf2d29ab7c5d15add1a720db31c3fc813ba54f073156c046f136bf1b690c2943",
-  reverbClientSecret: "fec256cf61a8797d3537c5816c532d0e714ec62688475a4dee0afe2903538cc2",
-  twilioAccountSID: "ACae93953dc9cf108cfcf316c27b67514f",
-  twilioAuthToken: "f6ac32262e73863ff7978427c4dbc3d9",
-  twilioPhoneNumber: "+16156516990",
-};
-
 module.exports = () => {
   let SMSclient = new require('twilio')(secrets.twilioAccountSID, secrets.twilioAuthToken);
   SMSclient.messages.create({
